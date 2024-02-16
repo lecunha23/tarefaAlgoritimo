3bd86a4cadff84fef8836c7bd4001350a4d8bbcd
lecunha23 <leandrojc23@gmail.com>
2024-02-16 20:38:59 -0300

    testanto

---
+++ Bubble_sort.rb
@@ -0,0 +1,16 @@
+
+def bubble_sort(arr):
+  n = len(arr)
+  # Traverse through all elements in the list
+  for i in range(n):
+      # Last i elements are already in place
+      for j in range(0, n-i-1):
+          # Traverse the list from 0 to n-i-1
+          # Swap if the element found is greater than the next element
+          if arr[j] > arr[j+1]:
+              arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array is:", arr)
